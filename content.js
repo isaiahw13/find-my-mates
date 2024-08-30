8b3a01d1060314f3dee1eeb3524cd8a608b61bd6
Isaiah White <119544336+isaiahw13@users.noreply.github.com>
2024-08-29 23:39:31 -0700

    Switched to generating buttons instead of opening windows

--- content.js
+++ content.js
@@ -1,42 +1,38 @@
-function getStudentNames() {
-  //get all student card elements from page
-  let users = document.getElementsByClassName(
+const LINKEDIN_URL = "https://www.linkedin.com/search/results/all/?keywords=";
+
+//Extracts the student's first and last name from the HTML element that contains it
+function getStudentName(studentCard) {
+  let studentFullName = studentCard.innerText.split(" ");
+
+  //return the student's name separated by first and last name
+  return { firstName: studentFullName[0], lastName: studentFullName.at(-1) };
+}
+
+//Creates the connect link button HTML elements for each user
+function generateConnectButtonHTML(studentName) {
+  //link to LinkedIn search of student's name
+  const link = `${LINKEDIN_URL}${studentName.firstName}%20${studentName.lastName}&origin=GLOBAL_SEARCH_HEADER`;
+  const linkedinIcon =
+    "https://upload.wikimedia.org/wikipedia/commons/c/ca/LinkedIn_logo_initials.png";
+  //return connect button element
+  return `
+  <a href="${link}" target="_blank">
+    <img src="${linkedinIcon}" alt="LinkedIn Button" style="width:24px; height:24px; margin-left:10px" >
+  </a>`;
+}
+
+//Inserts buttons next to each student's name with links to LinkedIn search for their name
+function insertConnectButtons() {
+  //get each student card element
+  let studentCards = document.getElementsByClassName(
     "roster_user_name student_context_card_trigger",
   );
 
-  //extract all names from student cards to an array
-  let userNames = [];
-  for (i of users) {
-    let fullName = i.innerText.split(" ");
-    userNames.push({
-      firstName: fullName[0],
-      lastName: fullName[1],
-    });
-  }
-
-  //return the array of student names
-  return userNames;
-}
-
-function openWindows(schoolName) {
-  let userNames = getStudentNames();
-  let linkedInURL = "https://www.linkedin.com/search/results/all/?keywords=";
-  let separatedSchoolName = schoolName.split(" ");
-
-  //open LinkedIn search windows for each student name
-  for (user of userNames) {
-    //generate search url
-    let searchURL =
-      linkedInURL + "%20" + user.firstName + "%20" + user.lastName;
-    for (i of separatedSchoolName) {
-      searchURL += "%20" + i;
-    }
-    searchURL += "&origin=GLOBAL_SEARCH_HEADER";
-
-    //open windows
-    window.open(searchURL, "_blank", "noopener");
-    setTimeout(1000);
+  //insert a connect button next to each student's name
+  for (student of studentCards) {
+    let html = generateConnectButtonHTML(getStudentName(student));
+    student.insertAdjacentHTML("afterend", html);
   }
 }
 
-openWindows("California State University East Bay");
+insertConnectButtons();
--- popup.js
+++ popup.js
@@ -1,12 +1,10 @@
 //Event listener to inject script
-document.addEventListener("click", () => {
+document.addEventListener("DOMContentLoaded", () => {
   //get current tab and inject script
   chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
-    console.log(tabs[0]);
     chrome.scripting.executeScript({
       target: { tabId: tabs[0].id },
       files: ["content.js"],
     });
   });
-  console.log("Script injected");
 });
